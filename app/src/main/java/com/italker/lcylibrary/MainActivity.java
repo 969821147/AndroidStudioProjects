0a0370f3dabdf8be8f4826770a62b92c93ffd955
969821147 <969821147@qq.com>
2018-09-03 09:21:25 +0800

    新建项目之下拉选择框

---
+++ app/src/main/java/com/italker/lcylibrary/MainActivity.java
@@ -0,0 +1,35 @@
+package com.italker.lcylibrary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.italker.lcylibrary.demo.DropDownActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn:
+                Intent intent = new Intent(this,DropDownActivity.class);
+                startActivity(intent);
+                break;
+            default:
+        }
+    }
+}
